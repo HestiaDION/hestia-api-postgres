c207613170b8391ed817feb90aa878b6d6d37687
laura4343 <123laurafarias@gmail.com>
2024-08-19 09:53:04 -0300

    first commit

---
+++ src/main/java/com/example/hestiaapipostgres/HestiaApiPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.hestiaapipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HestiaApiPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HestiaApiPostgresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hestiaapipostgres/HestiaApiPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hestiaapipostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HestiaApiPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
