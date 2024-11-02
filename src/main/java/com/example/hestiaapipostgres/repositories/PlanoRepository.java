54d1913073b24da1c2df129fef38200bd7652af5
laura4343 <123laurafarias@gmail.com>
2024-11-02 17:00:55 -0300

    registro de pagamento

--- src/main/java/com/example/hestiaapipostgres/repositories/PlanoRepository.java
+++ src/main/java/com/example/hestiaapipostgres/repositories/PlanoRepository.java
@@ -3,16 +3,18 @@ package com.example.hestiaapipostgres.repositories;
 
 import com.example.hestiaapipostgres.models.Plano;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PlanoRepository extends JpaRepository<Plano, UUID> {
 
-    Optional<Plano> findPlanoByTipo_plano(String tipoPlano);
+//    Optional<Plano> findPlanoByTipo_plano(String tipoPlano);
 
-    Optional<Plano> findPlanoById(UUID id);
+//    Optional<Plano> findPlanoById(UUID id);
 
-    Optional<Plano> findPlanoByUserEmail(String email);
-
+    @Query(value = "SELECT * FROM get_planos_by_user_email(:email)", nativeQuery = true)
+    Optional<Plano> findPlanoByUserEmail(@Param("email") String email);
 }
--- src/main/java/com/example/hestiaapipostgres/services/PagamentoService.java
+++ src/main/java/com/example/hestiaapipostgres/services/PagamentoService.java
@@ -1,11 +1,14 @@
 package com.example.hestiaapipostgres.services;
 
+import com.example.hestiaapipostgres.dto.InfoUserDTO;
 import com.example.hestiaapipostgres.dto.register.RegisterPagamentoDTO;
 import com.example.hestiaapipostgres.models.Pagamento;
 import com.example.hestiaapipostgres.models.Plano;
 import com.example.hestiaapipostgres.repositories.PagamentoRepository;
 import com.example.hestiaapipostgres.repositories.PlanoRepository;
 
+import com.example.hestiaapipostgres.repositories.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 
@@ -17,14 +20,21 @@ public class PagamentoService {
 
     private final PagamentoRepository pagamentoRepository;
     private final PlanoRepository planoRepository;
+    private final UsuarioRepository usuarioRepository;
 
-
-    public PagamentoService(PagamentoRepository pagamentoRepository, PlanoRepository planoRepository) {
+    public PagamentoService(PagamentoRepository pagamentoRepository, PlanoRepository planoRepository, UsuarioRepository usuarioRepository) {
         this.pagamentoRepository = pagamentoRepository;
         this.planoRepository = planoRepository;
+        this.usuarioRepository = usuarioRepository;
     }
 
     public Pagamento registerPagamento(RegisterPagamentoDTO pagamentoDTO) {
+
+        Optional<InfoUserDTO> infoUserOptional = usuarioRepository.findUserOriginByEmail(pagamentoDTO.emailUsuarioAssinante());
+        if (infoUserOptional.isEmpty()) {
+            throw new EntityNotFoundException("Este e-mail de usuário não existe");
+        }
+
         Optional<Plano> planoOptional = planoRepository.findPlanoByUserEmail(pagamentoDTO.emailUsuarioAssinante());
 
         if (planoOptional.isPresent()) {
@@ -38,8 +48,8 @@ public class PagamentoService {
 
             return pagamentoRepository.save(pagamento);
         } else {
-            throw new IllegalArgumentException("Plano não encontrado com ID: " + planoOptional.get().getId());
+            throw new IllegalArgumentException("Plano não encontrado para o e-mail: " + pagamentoDTO.emailUsuarioAssinante());
         }
-
     }
+
 }
