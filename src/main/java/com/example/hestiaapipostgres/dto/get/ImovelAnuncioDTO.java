632892c5594fec412bf9d7e512af981d38490706
laura4343 <123laurafarias@gmail.com>
2024-11-02 00:08:42 -0300

    mapping fields

--- src/main/java/com/example/hestiaapipostgres/dto/get/ImovelAnuncioDTO.java
+++ src/main/java/com/example/hestiaapipostgres/dto/get/ImovelAnuncioDTO.java
@@ -1,17 +1,25 @@
 package com.example.hestiaapipostgres.dto.get;
 
+import com.nimbusds.jose.shaded.gson.annotations.SerializedName;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record ImovelAnuncioDTO(
+        @SerializedName("imovel_id")
         UUID imovelId,
         String regras,
         String descricao,
+        @SerializedName("quantidade_quartos")
         int quantidadeQuartos,
+        @SerializedName("universidade_proxima")
         String universidadadeProxima,
+        @SerializedName("quantidade_maxima_pessoas")
         int quantidadeMaximaPessoas,
+        @SerializedName("nome")
         String nomeAnuncio,
         double aluguel,
+        @SerializedName("dt_inicio")
         LocalDate dtInicio
 
 ) {
