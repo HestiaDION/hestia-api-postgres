8ab7a113883f645006a7e1b57a3dcdcf8d8a08a4
laura4343 <123laurafarias@gmail.com>
2024-11-03 23:39:04 -0300

    token time implementing + persmisson routes

--- src/main/java/com/example/hestiaapipostgres/controllers/PagamentoController.java
+++ src/main/java/com/example/hestiaapipostgres/controllers/PagamentoController.java
@@ -13,10 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
@@ -29,7 +26,7 @@ public class PagamentoController {
      }
 
 //                  =-=-==-=--=-= POST ---==-=--=
-    @PostMapping("registerPayment")
+    @PostMapping("/registerPayment")
     @Operation(summary = "Registro de um pagamento",
             description = "Registra um pagamento no banco de dados")
     @ApiResponses(value = {
