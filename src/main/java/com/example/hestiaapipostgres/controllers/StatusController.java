ce75ec2af58d5aedbece27b721f415ca9b01cb91
sophiekumagai <sophie.satie20@gmail.com>
2024-11-05 10:50:40 -0300

    status controller

--- src/main/java/com/example/hestiaapipostgres/controllers/StatusController.java
+++ src/main/java/com/example/hestiaapipostgres/controllers/StatusController.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -21,7 +25,10 @@ public class StatusController {
             @ApiResponse(responseCode = "200", description = "Status retornado com sucesso!", content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content())
     })
-    public Map<String, String> getStatus() {
-        return Collections.singletonMap("status", "API is running");
+    public ResponseEntity<Map<String, String>> getStatus() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "API is running");
+
+        return new ResponseEntity<>(status, HttpStatus.OK);
     }
 }
