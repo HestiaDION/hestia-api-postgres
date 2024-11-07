9b397a0cfbfae2681e86227d320048cbeb384485
laura4343 <123laurafarias@gmail.com>
2024-11-06 22:46:47 -0300

    fix jwt

---
+++ src/main/java/com/example/hestiaapipostgres/dto/TokenAccess.java
@@ -0,0 +1,12 @@
+package com.example.hestiaapipostgres.dto;
+
+
+public record TokenAccess(
+        String token
+) {
+    public static TokenAccess fromToken(String token) {
+        return new TokenAccess(
+                token == null ? null : "Bearer " + token
+        );
+    }
+}
